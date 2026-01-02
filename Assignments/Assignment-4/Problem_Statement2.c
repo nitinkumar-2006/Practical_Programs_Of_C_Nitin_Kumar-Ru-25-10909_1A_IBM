15e007356dc3f21f34f9451edd2c80787bc17eea
Nitin Kumar <nitin.kumar@rungta.org>
2026-01-02 19:11:29 +0530

    Add files via upload

---
+++ Assignments/Assignment-4/Problem_Statement2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int max( int *a , int *b)
+{ 
+    if( *a > *b)
+    return *a; 
+    else
+    return *b;
+}
+int main(){
+    int a1,b1,*max1;
+    printf("Enter first number:");
+    scanf("%d",&a1);
+    printf("Enter second number:");
+    scanf("%d",&b1);
+    *max1=max(&a1,&b1);
+    printf("Greater number is:\t%d",*max1);
+    return 0;
+}
+//Code to find the maximum of two numbers using pointers in C
