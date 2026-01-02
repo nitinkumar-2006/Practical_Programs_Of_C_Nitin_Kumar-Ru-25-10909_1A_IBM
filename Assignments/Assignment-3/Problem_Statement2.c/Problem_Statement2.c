943647fe58e28db3abb54555139442abba785ef9
Nitin Kumar <nitin.kumar@rungta.org>
2026-01-02 17:38:12 +0530

    Add files via upload

---
+++ Assignments/Assignment-3/Problem_Statement2.c/Problem_Statement2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    for (i = 1; i <= 7; i++) {
+        for (j = 1; j <= 7; j++) {
+            if (j == 1 || j == 7 || j == i) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("   ");
+
+        for (j = 1; j <= 7; j++) {
+            if (i == 1 || i == 7 || j == 4) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("   ");
+
+        for (j = 1; j <= 7; j++) {
+            if (i == 1 || j == 4) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("   ");
+
+        for (j = 1; j <= 7; j++) {
+            if (i == 1 || i == 7 || j == 4) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("   ");
+
+        for (j = 1; j <= 7; j++) {
+            if (j == 1 || j == 7 || j == i) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\t");
+
+        
+        printf("\n");
+    }
+
+    return 0;
+}
